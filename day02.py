ba022817240e6f0dd664a6266c79440f2484d867
Chris Brooks <chris@chrisbrooks.org>
2019-12-02 17:13:58 -0800

    Made compute function string aware

--- day02.py
+++ day02.py
@@ -1,12 +1,3 @@
-def test_halt():
-    assert compute("99") == "99"
-
-
-def test_several():
-    assert compute("1,0,0,0,99") == "2,0,0,0,99"
-    assert compute("1,1,1,4,99,5,6,0,99") == "30,1,1,4,2,5,6,0,99"
-
-
 def concat(op_list):
     result = ""
     for o in op_list:
@@ -14,8 +5,17 @@ def concat(op_list):
     return result[:-1]
 
 
-def compute(op_list):
-    # code_list = [int(x) for x in op_string.split(",")]
+def create_code_list(input):
+    return [int(x) for x in input.split(",")]
+
+
+def create_code_string(code_list):
+    return ','.join(map(str, code_list))
+
+
+def compute(code_list):
+    if isinstance(code_list, str):
+        code_list = create_code_list(code_list)
     i = 0
     while True:
         base = 4 * i
@@ -25,26 +25,35 @@ def compute(op_list):
             break
         # Addition
         if op == 1:
-            a = code_list[code_list[base+1]]
-            b = code_list[code_list[base+2]]
-            code_list[code_list[base+3]] = a + b
+            a = code_list[code_list[base + 1]]
+            b = code_list[code_list[base + 2]]
+            code_list[code_list[base + 3]] = a + b
         # Multply
         if op == 2:
-            a = code_list[code_list[base+1]]
-            b = code_list[code_list[base+2]]
-            code_list[code_list[base+3]] = a * b
+            a = code_list[code_list[base + 1]]
+            b = code_list[code_list[base + 2]]
+            code_list[code_list[base + 3]] = a * b
         i = i + 1
     return code_list
 
 
-input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,5,23,2,9,23,27,1,5,27,31,1,5,31,35,1,35,13,39,1,39,9,43,1,5,43,47,1,47,6,51,1,51,13,55,1,55,9,59,1,59,13,63,2,63,13,67,1,67,10,71,1,71,6,75,2,10,75,79,2,10,79,83,1,5,83,87,2,6,87,91,1,91,6,95,1,95,13,99,2,99,13,103,1,103,9,107,1,10,107,111,2,111,13,115,1,10,115,119,1,10,119,123,2,13,123,127,2,6,127,131,1,13,131,135,1,135,2,139,1,139,6,0,99,2,0,14,0"
+assert compute("99") == [99]
+assert compute("1,0,0,0,99") == [2, 0, 0, 0, 99]
+assert compute("1,1,1,4,99,5,6,0,99") == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+
+input = """1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,5,23,2,9,23,27,1,5,27,
+31,1,5,31,35,1,35,13,39,1,39,9,43,1,5,43,47,1,47,6,51,1,51,13,55,1,55,9,59,1,59,
+13,63,2,63,13,67,1,67,10,71,1,71,6,75,2,10,75,79,2,10,79,83,1,5,83,87,2,6,87,91,
+1,91,6,95,1,95,13,99,2,99,13,103,1,103,9,107,1,10,107,111,2,111,13,115,1,10,115,
+119,1,10,119,123,2,13,123,127,2,6,127,131,1,13,131,135,1,135,2,139,1,139,6,0,99,
+2,0,14,0"""
 
 
 for noun in range(99):
     for verb in range(99):
-        code_list = [int(x) for x in input.split(",")]
+        code_list = create_code_list(input)
         code_list[1] = noun
         code_list[2] = verb
-        if compute(input)[0] == 19690720:
+        if compute(code_list)[0] == 19690720:
             print(f"{noun} * {verb} = {100 * noun + verb}")
             break
