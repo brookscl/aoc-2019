f638307224cac89aeac51f337a8bad7180e95031
Chris Brooks <chris@chrisbrooks.org>
2019-12-03 21:34:51 -0800

    Part 2 done

--- day04.py
+++ day04.py
@@ -1,3 +1,5 @@
+import re
+
 
 def is_password(p):
     if not ''.join(sorted(p)) == p:
@@ -10,6 +12,18 @@ def is_password(p):
     return False
 
 
+def is_password2(p):
+    if not ''.join(sorted(p)) == p:
+        return False
+
+    groups = [m.group(0) for m in re.finditer(r"(\d)\1*", p)]
+    for g in groups:
+        if len(g) == 2:
+            return True
+
+    return False
+
+
 assert is_password("111111")
 assert not is_password("223450")
 assert not is_password("123789")
@@ -17,6 +31,17 @@ assert not is_password("123789")
 count = 0
 for i in range(124075, 580769+1):
     if is_password(str(i)):
+        count += 1
+
+print(count)
+
+assert is_password2("112233")
+assert not is_password2("123444")
+assert is_password2("111122")
+
+count = 0
+for i in range(124075, 580769+1):
+    if is_password2(str(i)):
         count += 1
 
 print(count)
