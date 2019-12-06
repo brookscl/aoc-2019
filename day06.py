5f0834a274e3a62dfbb191a6af71421394df1bf3
Chris Brooks <chris@chrisbrooks.org>
2019-12-05 23:03:19 -0800

    Completed part 2

--- day06.py
+++ day06.py
@@ -1,8 +1,20 @@
 from collections import deque
 
-
-orbits = ["COM)B", "B)C"]
-
+def find_parents(tag, orbit_graph):
+    parent_list = []
+    current_node = tag
+    while current_node != 'COM':
+        found = False
+        for parent, children in orbit_graph.items():
+            for child in children:
+                if child == current_node:
+                    parent_list.append(parent)
+                    current_node = parent
+                    found = True
+                    break
+        if not found:
+            break
+    return parent_list
 
 def total_orbits(orbits):
     orbit_graph = {}
@@ -24,8 +36,6 @@ def total_orbits(orbits):
     queue.append(start)
     levels = {}
     levels[start] = 0
-    direct = 0
-    indirect = 0
 
     while queue:
         node = queue.pop()
@@ -35,19 +45,48 @@ def total_orbits(orbits):
                 neighbors = orbit_graph[node]
             else:
                 neighbors = []
-            direct += len(neighbors)
             for n in neighbors:
-                indirect += 1
                 levels[n] = levels[node] + 1
                 queue.appendleft(n)
-    print(f"Total nodes: {len(levels)}")
 
-    print(f"Other calc: {direct + indirect}")
+    # Calc santa distance
+    you_parents = find_parents("YOU", orbit_graph)
+    santa_parents = find_parents("SAN", orbit_graph)
+    common_parents = list(set(you_parents) & set(santa_parents))
+    closest_parent = -1
+    for p in common_parents:
+        if levels[p] > closest_parent:
+            closest_parent = levels[p]
+
+    print(f"Santa distance: {levels['YOU'] + levels['SAN'] - 2 * closest_parent - 2}")
+
     return sum(levels.values())
 
 
-assert total_orbits(orbits) == 3
+orbits = """COM)B
+B)C
+C)D
+D)YOU
+D)E
+E)F
+B)G
+G)H
+E)J
+J)K
+K)SAN"""
+orbits = orbits.split("\n")
+print(total_orbits(orbits))
+assert total_orbits(orbits) == 42
 
+
+with open("orbits.txt") as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+orbits = [x.strip() for x in content]
+
+print(total_orbits(orbits))
+
+# Part 2
 
 orbits = """COM)B
 B)C
@@ -59,23 +98,8 @@ G)H
 D)I
 E)J
 J)K
-K)L"""
+K)L
+K)YOU
+I)SAN"""
 orbits = orbits.split("\n")
-print(total_orbits(orbits))
-assert total_orbits(orbits) == 42
-
-orbits = """COM)A
-A)B
-A)C
-C)D
-C)E"""
-orbits = orbits.split("\n")
-print(total_orbits(orbits))
-assert total_orbits(orbits) == 11
-
-with open("orbits.txt") as f:
-    content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-orbits = [x.strip() for x in content]
-
 print(total_orbits(orbits))
