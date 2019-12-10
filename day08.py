7b39a4388fc3e7b190fcde598ea6719ef8ba2d91
Chris Brooks <chris@chrisbrooks.org>
2019-12-10 06:14:21 -0800

    More linting

--- day08.py
+++ day08.py
@@ -1,8 +1,7 @@
 
 
-
 def find_zero_layer(image, layer_size):
-    layers = [image[i:i+layer_size] for i in range(0, len(image), layer_size)]
+    layers = [image[i: i + layer_size] for i in range(0, len(image), layer_size)]
     check_zeros = {}
     for layer in layers:
         check_zero = layer.count('0')
@@ -10,6 +9,7 @@ def find_zero_layer(image, layer_size):
         check_zeros[check_ones_twos] = check_zero
 
     return min(check_zeros, key=check_zeros.get)
+
 
 test_image = "123456789012"
 
@@ -31,12 +31,12 @@ print(find_zero_layer(image, 150))
 def collapse(image, width, height):
     final_image = [['2' for x in range(width)] for x in range(height)]
     layer_size = width * height
-    layers = [image[i:i+layer_size] for i in range(0, len(image), layer_size)]
+    layers = [image[i: i + layer_size] for i in range(0, len(image), layer_size)]
     for layer in layers:
         for row in range(height):
             for col in range(width):
                 if final_image[row][col] == '2':
-                    final_image[row][col] = layer[row*width + col]
+                    final_image[row][col] = layer[row * width + col]
 
     return final_image
 
@@ -52,6 +52,7 @@ def render(image, width, height):
         print('  ', flush=True)
 
 # render("0222112222120000", 2, 2)
+
 
 width = 25
 height = 6
