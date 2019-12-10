5705e62bee90eb5d39e7702b72656964e7e410d0
Chris Brooks <chris@chrisbrooks.org>
2019-12-09 20:35:45 -0800

    First refactoring

--- day07.py
+++ day07.py
@@ -13,8 +13,7 @@ OP_CODES = {99: 0, 1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4}
 
 
 class ComputerState:
-    def __init__(self, modes, pc, code_list):
-        self.modes = modes
+    def __init__(self, pc, code_list):
         self.pc = pc
         self.code_list = code_list
 
@@ -24,97 +23,107 @@ class ComputerState:
         else:  # immediate mode
             return self.code_list[self.pc + param]
 
+    def fetch_op(self):
+        op = self.code_list[self.pc]
+        str_op = str(op).zfill(5)
+        self.op = int(str_op[-2:])
+        self.modes = [int(str_op[-3]), int(str_op[-4])]
+
+    def increment_pc(self):
+        increment = OP_CODES[self.op]
+        self.pc += increment
+
+    def run_op(self, input_entry):
+        output = None
+        # Halt
+        self.break_out = False
+        if self.op == 99:
+            output = input_entry[0]
+            self.halted = True
+            self.break_out = True
+        # Addition
+        if self.op == 1:
+            a = self.get_param(1)
+            b = self.get_param(2)
+            self.code_list[self.code_list[self.pc + 3]] = a + b
+            self.increment_pc()
+        # Multiply
+        if self.op == 2:
+            a = self.get_param(1)
+            b = self.get_param(2)
+            self.code_list[self.code_list[self.pc + 3]] = a * b
+            self.increment_pc()
+        # Input
+        if self.op == 3:
+            if input_entry is None:
+                input_entry = [int(input("Enter the integer input:"))]
+            self.code_list[self.code_list[self.pc + 1]] = input_entry[0]
+            input_entry.pop(0)
+            self.increment_pc()
+        # Output
+        if self.op == 4:
+            output = self.get_param(1)
+            # print(f"OUTPUT: {output} for amp {amp}")
+            # Save state
+            self.halted = False
+            self.increment_pc()
+            self.break_out = True
+        # Opcode 5 is jump-if-true
+        if self.op == 5:
+            if self.get_param(1):
+                self.pc = self.get_param(2)
+            else:
+                self.increment_pc()
+        # Opcode 6 is jump-if-false
+        if self.op == 6:
+            if not self.get_param(1):
+                self.pc = self.get_param(2)
+            else:
+                self.increment_pc()
+        # Opcode 7 is less than
+        if self.op == 7:
+            if self.get_param(1) < self.get_param(2):
+                self.code_list[self.code_list[self.pc + 3]] = 1
+            else:
+                self.code_list[self.code_list[self.pc + 3]] = 0
+            self.increment_pc()
+        # Opcode 8 is equals
+        if self.op == 8:
+            if self.get_param(1) == self.get_param(2):
+                self.code_list[self.code_list[self.pc + 3]] = 1
+            else:
+                self.code_list[self.code_list[self.pc + 3]] = 0
+            self.increment_pc()
+
+        return output
+
     halted = False
 
 
 def compute(code_list, input_entry=None, amp=None, state=None):
-    if isinstance(code_list, str):
-        code_list = create_code_list(code_list)
     output = None
-    if state:
-        pc = state.pc
-        # pc = 0
-        code_list = state.code_list
-    else:
-        pc = 0
+    if not state:
+        state = ComputerState(0, code_list)
 
     while True:
-        op = code_list[pc]
-        str_op = str(op).zfill(5)
-        op = int(str_op[-2:])
-        modes = [int(str_op[-3]), int(str_op[-4])]
-        c = ComputerState(modes, pc, code_list)
-        increment = OP_CODES[op]
-        # Halt
-        if op == 99:
-            output = input_entry[0]
-            c.halted = True
+        state.fetch_op()
+
+        output = state.run_op(input_entry)
+        if state.break_out:
             break
-        # Addition
-        if op == 1:
-            a = c.get_param(1)
-            b = c.get_param(2)
-            code_list[code_list[pc + 3]] = a + b
-            pc += increment
-        # Multply
-        if op == 2:
-            a = c.get_param(1)
-            b = c.get_param(2)
-            code_list[code_list[pc + 3]] = a * b
-            pc += increment
-        # Input
-        if op == 3:
-            if input_entry is None:
-                input_entry = [int(input("Enter the integer input:"))]
-            code_list[code_list[pc + 1]] = input_entry[0]
-            input_entry.pop(0)
-            pc += increment
-        # Output
-        if op == 4:
-            output = c.get_param(1)
-            # print(f"OUTPUT: {output} for amp {amp}")
-            # Save state
-            c.halted = False
-            pc += increment
-            c.pc = pc
-            c.code_list = code_list
-            break
-        # Opcode 5 is jump-if-true
-        if op == 5:
-            if c.get_param(1):
-                pc = c.get_param(2)
-            else:
-                pc += increment
-        # Opcode 6 is jump-if-false
-        if op == 6:
-            if not c.get_param(1):
-                pc = c.get_param(2)
-            else:
-                pc += increment
-        # Opcode 7 is less than
-        if op == 7:
-            if c.get_param(1) < c.get_param(2):
-                code_list[code_list[pc + 3]] = 1
-            else:
-                code_list[code_list[pc + 3]] = 0
-            pc += increment
-        # Opcode 8 is equals
-        if op == 8:
-            if c.get_param(1) == c.get_param(2):
-                code_list[code_list[pc + 3]] = 1
-            else:
-                code_list[code_list[pc + 3]] = 0
-            pc += increment
-    return (output, c)
+
+    return (output, state)
 
 
-def multiphase(program, phases):
+def multiphase(program_string, phases):
     if isinstance(phases, str):
         phases = list(map(int, phases.split(",")))
     staged_input = 0
     amp = 0
     states = {}
     while True:
+        program = create_code_list(program_string)
+
         state = None
         if amp in states:
             state = states[amp]
@@ -134,7 +143,8 @@ program = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 
 assert multiphase(program, "4,3,2,1,0") == 43210
 
-program = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+program = """3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,
+33,31,31,1,32,31,31,4,31,99,0,0,0"""
 
 assert multiphase(program, "1,0,4,3,2") == 65210
 
@@ -158,11 +168,14 @@ print(f"Max output is: {max_output}")
 
 # Part 2
 
-program = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+program = """3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,
+28,6,99,0,0,5"""
 
 assert multiphase(program, "9,8,7,6,5") == 139629729
 
-program = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+program = """3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,
+54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,
+1005,56,6,99,0,0,0,0,10"""
 
 assert multiphase(program, "9,7,8,5,6") == 18216
 
