9ff6ba914bc776c41e590fa7715ee2f9a18e5af8
Chris Brooks <chris@chrisbrooks.org>
2019-12-09 18:28:00 -0800

    Cleaning up before refactoring

--- day07.py
+++ day07.py
@@ -108,12 +108,9 @@ def compute(code_list, input_entry=None, amp=None, state=None):
     return (output, c)
 
 
-# assert compute("3,0,4,0,99", [1]) == 1
-
-
 def multiphase(program, phases):
     if isinstance(phases, str):
-        phases = list(map(int, phases.split(',')))
+        phases = list(map(int, phases.split(",")))
     staged_input = 0
     amp = 0
     states = {}
@@ -130,45 +127,44 @@ def multiphase(program, phases):
         states[amp] = state
         amp = (amp + 1) % len(phases)
 
-
     return staged_input
 
 
-# program = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+program = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 
-# assert multiphase(program, "4,3,2,1,0") == 43210
+assert multiphase(program, "4,3,2,1,0") == 43210
 
-# program = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+program = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
 
-# assert multiphase(program, "1,0,4,3,2") == 65210
+assert multiphase(program, "1,0,4,3,2") == 65210
 
-# # Part 1
+# Part 1
 
-# with open("day07_program.txt") as f:
-#     content = f.readlines()
+with open("day07_program.txt") as f:
+    content = f.readlines()
 
-# program = content[0].strip()
+program = content[0].strip()
 
-# phase_options = list(itertools.permutations([0, 1, 2, 3,4]))
+phase_options = list(itertools.permutations([0, 1, 2, 3, 4]))
 
-# max_output = -1
+max_output = -1
 
-# for phase_check in phase_options:
-#     output = multiphase(program, phase_check)
-#     if output > max_output:
-#         max_output = output
+for phase_check in phase_options:
+    output = multiphase(program, phase_check)
+    if output > max_output:
+        max_output = output
 
-# print(max_output)
+print(f"Max output is: {max_output}")
 
 # Part 2
 
-# program = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+program = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
 
-# print(multiphase(program, "9,8,7,6,5"))
+assert multiphase(program, "9,8,7,6,5") == 139629729
 
-# program = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+program = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
 
-# print(multiphase(program, "9,7,8,5,6"))
+assert multiphase(program, "9,7,8,5,6") == 18216
 
 
 with open("day07_program.txt") as f:
