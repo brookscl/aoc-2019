e8aa7ac060e72c2def1be9a2911c842422905154
Chris Brooks <chris@chrisbrooks.org>
2019-12-12 12:21:16 -0500

    Part 2 done

--- day10.py
+++ day10.py
@@ -1,6 +1,10 @@
 from collections import Counter, namedtuple
+from math import atan2, degrees, pi
+import operator
+
 
 Point = namedtuple("Point", "x y")
+PolarCoord = namedtuple("PolarCoord", "angle distance")
 
 
 def load_asteroid_map(map):
@@ -143,7 +147,7 @@ print(a)
 assert a[0] == Point(1, 2)
 assert a[1] == 35
 
-map = """.#..##.###...#######
+big_map = """.#..##.###...#######
 ##.############..##.
 .#.######.########.#
 .###.#######.####.#.
@@ -164,15 +168,15 @@ map = """.#..##.###...#######
 #.#.#.#####.####.###
 ###.##.####.##.#..##"""
 
-asteroids = load_asteroid_map(map)
-a = best_asteroid(asteroids)
-print(a)
-assert a[0] == Point(11, 13)
-assert a[1] == 210
+# asteroids = load_asteroid_map(map)
+# a = best_asteroid(asteroids)
+# print(a)
+# assert a[0] == Point(11, 13)
+# assert a[1] == 210
 
 # Part 1
 
-map = """.............#..#.#......##........#..#
+real_map = """.............#..#.#......##........#..#
 .#...##....#........##.#......#......#.
 ..#.#.#...#...#...##.#...#.............
 .....##.................#.....##..#.#.#
@@ -213,7 +217,108 @@ map = """.............#..#.#......##........#..#
 ..............##.#.#.#...........#....."""
 
 asteroids = load_asteroid_map(map)
-a = best_asteroid(asteroids)
-print(a)
-assert a[0] == Point(26, 29)
-assert a[1] == 299
+# a = best_asteroid(asteroids)
+# print(a)
+# assert a[0] == Point(26, 29)
+# assert a[1] == 299
+
+# Part 2
+
+
+def polar(start, end):
+    dist = distance(start, end)
+    angle = degrees(atan2(end.x - start.x, start.y - end.y))
+    if angle < 0:
+        angle += 360.0
+    return PolarCoord(angle, dist)
+
+
+def vaporize(asteroids, base):
+    # Collect polar coordinates for all other asteroids
+    polars = {}
+    for a in asteroids - {base}:
+        polars[a] = polar(base, a)
+    vapor_ordered = sorted(polars.items(), key=lambda p: (p[1].angle, p[1].distance))
+    print(
+        f"Polar coordinate list processed, {len(vapor_ordered)} asteroids to destroy."
+    )
+    return vapor_ordered
+
+
+def process_canon(canon, vaporize_list):
+    vap_seq = []
+    i = 0
+    while vaporize_list:
+        a = vaporize_list[i]
+        vap_seq.append(a)
+        del vaporize_list[i]
+        if len(vaporize_list) == 0:
+            break
+        i = i % len(vaporize_list)
+        if (len(vaporize_list) % 10) == 0:
+            print(f"Vaporizing, at {len(vaporize_list)} to prioritize")
+        if len(vaporize_list) < 10:
+            print(f"  Count-down, at {len(vaporize_list)} to prioritize")
+
+        # If next item in list is colinear, skip it
+        if len(vaporize_list) > 1:
+            while colinear(canon, a[0], vaporize_list[i][0]):
+                i = i + 1
+                if i >= len(vaporize_list):
+                    i = 0
+                    break
+    print(f"Canon vaporize list processed, {len(vap_seq)} asteroids to destroy.")
+    return vap_seq
+
+
+map = """.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....#...###..
+..#.#.....#....##"""
+
+a = Point(8, 3)
+b = Point(8, 1)
+p = polar(a, b)
+print(p)
+assert p == PolarCoord(0.0, 2.0)
+p = polar(b, a)
+print(p)
+assert p == PolarCoord(180.0, 2.0)
+c = Point(9, 0)
+p = polar(a, c)
+assert 0 < p.angle < 20.0
+
+asteroids = load_asteroid_map(map)
+laser_base = best_asteroid(asteroids)[0]
+print(laser_base)
+assert laser_base == Point(8, 3)
+vaporize_todo_list = vaporize(asteroids, laser_base)
+vaporize_sequence = process_canon(laser_base, vaporize_todo_list)
+first_vaporized = vaporize_sequence[0]
+assert first_vaporized[0] == Point(8, 1)
+assert [p[0] for p in vaporize_sequence[:5]] == [
+    Point(8, 1),
+    Point(9, 0),
+    Point(9, 1),
+    Point(10, 0),
+    Point(9, 2),
+]
+
+# print("Big map time...")
+# asteroids = load_asteroid_map(big_map)
+# laser_base = best_asteroid(asteroids)[0]
+# print(laser_base)
+# assert laser_base == Point(11, 13)
+# vaporize_todo_list = vaporize(asteroids, laser_base)
+# vaporize_sequence = process_canon(laser_base, vaporize_todo_list)
+# first_vaporized = vaporize_sequence[0]
+# assert first_vaporized[0] == Point(11, 12)
+
+asteroids = load_asteroid_map(real_map)
+laser_base = best_asteroid(asteroids)[0]
+print(laser_base)
+# assert laser_base == Point(11, 13)
+vaporize_todo_list = vaporize(asteroids, laser_base)
+vaporize_sequence = process_canon(laser_base, vaporize_todo_list)
+print(f"200th asteroid: {vaporize_sequence[199]}")
