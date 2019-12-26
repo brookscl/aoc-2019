4300566a838406daa483410be290f34a4ff51b6b
Chris Brooks <chris@chrisbrooks.org>
2019-12-26 08:30:40 -0500

    Working part 2

--- day16.py
+++ day16.py
@@ -2,84 +2,40 @@ import math
 import numpy
 import time
 
-input_signal = "12345678"
-
 PATTERN = [0, 1, 0, -1]
 PATTERN_LENGTH = 4
 
 
 def fft(signal, phases, extend=1):
-    # extend pattern to be at least as long as signal
     signal_length = len(signal)
-    # extend = math.ceil(len(signal) / len(PATTERN)) + 1
-    # pattern = PATTERN * extend
+    places_to_compute = signal_length * extend
 
     for phase in range(phases):
         output = ""
         signal_list = [int(d) for d in signal]
-        for i in range(signal_length * extend):
-            # Hmmm... pattern can be generated on the fly rather
-            # than pre-building an array.
-            # How long will pattern be zero at start? Can start
-            # range there.
-
-            # p = numpy.repeat(pattern, i + 1)[1 : signal_length + 1]
-            # product = numpy.multiply(signal_list, p)
-            # sum = numpy.sum(product)
-
+        for i in range(places_to_compute):
             # Instead of multiplying, let's add/subtract
             sum = 0
-            for j in range(i, signal_length * extend):
-                p = PATTERN[(((j + 1) // (i + 1))) % PATTERN_LENGTH]
-                if p == -1:
-                    sum -= signal_list[j % signal_length]
-                elif p == 1:
-                    sum += signal_list[j % signal_length]
+            j = i
+            while j < signal_length * extend:
+                for spread in range(i + 1):
+                    if (j + spread) < (signal_length * extend):
+                        sum += signal_list[(j + spread) % signal_length]
+                j += 4 * (i + 1)
+
+            # Move this inside loop above
+            j = 3 * i + 2
+            while j < signal_length * extend:
+                for spread in range(i + 1):
+                    if (j + spread) < (signal_length * extend):
+                        sum -= signal_list[(j + spread) % signal_length]
+                j += 4 * (i + 1)
             output += str(sum)[-1]
         signal = output
     return output[:8]
 
 
-def fft_with_offset(signal, phases, extend=1000):
-    # extend pattern to be at least as long as signal
-    signal = signal * extend
-    signal_length = len(signal)
-    offset = int(signal[:7])
-    # extend = math.ceil(len(signal) / len(PATTERN)) + 1
-    # pattern = PATTERN * extend
-
-    for phase in range(phases):
-        output = ""
-        signal_list = [int(d) for d in signal]
-        for i in range(signal_length):
-            # Hmmm... pattern can be generated on the fly rather
-            # than pre-building an array.
-            # How long will pattern be zero at start? Can start
-            # range there.
-
-            # p = numpy.repeat(pattern, i + 1)[1 : signal_length + 1]
-            # product = numpy.multiply(signal_list, p)
-            # sum = numpy.sum(product)
-
-            # Instead of multiplying, let's add/subtract
-            sum = 0
-            for j in range(i, signal_length * extend):
-                p = PATTERN[(((j + 1) // (i + 1))) % PATTERN_LENGTH]
-                if p == -1:
-                    sum -= signal_list[j % signal_length]
-                elif p == 1:
-                    sum += signal_list[j % signal_length]
-            output += str(sum)[-1]
-        signal = output
-        print(f"Phase {phase} completed, signal={signal}")
-    return signal[offset : offset + 8]
-
-
-def produce_output_with_offset(signal):
-    offset = int(signal[:7])
-    return signal[offset : offset + 8]
-
-
+input_signal = "12345678"
 output_signal = fft(input_signal, 1)
 assert output_signal == "48226158"
 
@@ -96,17 +52,53 @@ assert output_signal == "24176176"
 
 # Part 1
 
-# with open("day16_input.txt") as f:
-#     content = f.readlines()
+with open("day16_input.txt") as f:
+    content = f.readlines()
 
-# real_input_signal = content[0].strip()
-# start_time = time.time()
-# output_signal = fft(real_input_signal, 100)
-# print("--- %s seconds ---" % (time.time() - start_time))
-# assert output_signal == "94960436"
-# print(f"Output: {output_signal}")
+real_input_signal = content[0].strip()
+start_time = time.time()
+output_signal = fft(real_input_signal, 100)
+print("--- %s seconds ---" % (time.time() - start_time))
+assert output_signal == "94960436"
+print(f"Output: {output_signal}")
 
 # Part 2
+
+
+def quick_fft(input_signal):
+    offset = int(input_signal[:7])
+    signal_list = [int(d) for d in input_signal]
+    signal_list = signal_list * 10000
+    signal_list = signal_list[offset:]
+
+    phases = 100
+    for phase in range(phases):
+        output = []
+        s = sum(signal_list)
+        for i in range(len(signal_list)):
+
+            output.append(int(str(s)[-1]))
+            s -= signal_list[i]
+        signal_list = output
+        # print(f"Phase {phase} completed, signal: {signal_list[:8]}")
+    return "".join(str(x) for x in signal_list[:8])
+
+
+# 03036732577212944063491565474664 becomes 84462026
+
+
+# The starting offset is barely smaller than the length of the signal
+
+
 input_signal = "03036732577212944063491565474664"
-output = fft_with_offset(input_signal, 100, 10000)
+output = quick_fft(input_signal)
 assert output == "84462026"
+
+input_signal = "02935109699940807407585447034323"
+output = quick_fft(input_signal)
+assert output == "78725270"
+
+start_time = time.time()
+output_signal = quick_fft(real_input_signal)
+print("--- %s seconds ---" % (time.time() - start_time))
+print(f"Output: {output_signal}")
